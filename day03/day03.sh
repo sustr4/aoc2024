37ce97be8e9f74fac47d16247329d43396d4ed4c
Zdenek Sustr <git@sustr.net>
2024-12-03 06:23:27 +0100

    Day 3, task 2

--- day03/day03.sh
+++ day03/day03.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-grep -o 'mul([0-9][0-9]*,[0-9][0-9]*)' input.txt | awk -F'[(,)]' 'BEGIN {sum=0} {sum += $2 * $3} END {print sum}'
+grep -o -e 'mul([0-9][0-9]*,[0-9][0-9]*)' -e 'do()' -e "don't()" input.txt | while read l; do
+	if [ "$l" == "do()" ]; then
+		DO=""
+	elif [ "$l" == "don't()" ]; then
+		DO="not"
+	else
+		if [ "$DO" == "" ]; then
+			echo $l
+		fi
+	fi
+done | awk -F'[(,)]' 'BEGIN {sum=0} {sum += $2 * $3} END {print sum}'
 
