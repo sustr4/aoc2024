897efe1a0ae5a165759ecb48c5109d09f9caaab3
Zdenek Sustr <git@sustr.net>
2024-12-04 11:01:23 +0100

    Day 4, Task 2

--- day04/day04.c
+++ day04/day04.c
@@ -17,25 +17,6 @@
 //#define MAXX 6
 //#define MAXY 5
 
-// Point structure definition
-typedef struct {
-	int x;
-	int y;
-	int z;
-} TPoint;
-
-// Comparator function example
-int comp(const void *a, const void *b)
-{
-	const int *da = (const int *) a;
-	const int *db = (const int *) b;
-	return (*da > *db) - (*da < *db);
-}
-
-// Example for calling qsort()
-//qsort(array,count,sizeof(),comp);
-
-
 // Print a two-dimensional array
 void printMap (char **map) {
 	int x,y;
@@ -46,12 +27,10 @@ void printMap (char **map) {
 		printf("\n");
 	}
 }
-// Full block character for maps █ and border elements ┃━┗┛┏┓
-// Color printf("\033[1;31mR \033[1;32mG \033[1;34mB \033[0moff\n");
 
 // Retrieve nth neighbor from a map
-int dy[] = { -1, -1, -1, 0, 0, 1, 1, 1};
-int dx[] = { -1, 0, 1, -1, 1, -1, 0, 1};
+int dy[] = { -1, -1, -1, 0, 1, 1, 1, 0};
+int dx[] = { -1, 0, 1, 1, 1, 0, -1, -1};
 char mapnb(char **map, int y, int x, int n) {
 	assert((n>=0) && (n<8));
 	if((y+dy[n]<0) || (y+dy[n]>=MAXY) ||
@@ -73,10 +52,6 @@ char **readInput() {
 		fprintf(stderr,"Failed to open input file\n");
 		exit(1); }
 
-	// Allocate one-dimensional array of strings
-	// char **inst=(char**)calloc(MAXX, sizeof(char*));
-	// TPoint *inst=(TPoint*)calloc(MAXX, sizeof(TPoint));
-
 	// Allocate a two-dimensional arrray of chars
 	int x=0, y=0;
 	char **map=calloc(MAXY,sizeof(char*));
@@ -89,25 +64,8 @@ char **readInput() {
 		for(x=0; x<MAXX; x++) map[y][x] = line[x];
 		y++;
 
-		// Copy to string
-		//asprintf(&(inst[count]), "%s", line);	
-
-		// Read into array
-		// sscanf(line,"%d,%d",
-		//	&(inst[count].x),
-		//	&(inst[count].y));
-
-		// Read tokens from single line
-		//char *token;
-		//token = strtok(line, ",");
-		//while( 1 ) {
-		//	if(!(token = strtok(NULL, ","))) break;
-		//}
-
 		count++;
 	}
-	printMap(map);
-	printf("\n");
 
 	fclose(input);
 	if (line)
@@ -118,41 +76,25 @@ char **readInput() {
 
 int main(int argc, char *argv[]) {
 
-//	TPoint *array;
-//	int i=0;	
-//	array = readInput();
 	char **map=readInput();
 
-	printMap(map);
-	printf("\n");
-
-//	#pragma omp parallel for private(<uniq-var>) shared(<shared-var>)
 	int count=0;
 	int x,y;
-	for(y=0; y<MAXY; y++) {
-		for(x=0; x<MAXX; x++) {
-			int oldc=count;
-			if((x<MAXX-3) && (map[y][x]=='X') && (map[y][x+1]=='M') && (map[y][x+2]=='A') && (map[y][x+3]=='S')) count++;
-			if((x>2) && (map[y][x]=='X') && (map[y][x-1]=='M') && (map[y][x-2]=='A') && (map[y][x-3]=='S')) count++;
-			if((y<MAXY-3) && (map[y][x]=='X') && (map[y+1][x]=='M') && (map[y+2][x]=='A') && (map[y+3][x]=='S')) count++;
-			if((y>2) && (map[y][x]=='X') && (map[y-1][x]=='M') && (map[y-2][x]=='A') && (map[y-3][x]=='S')) count++;
-
-			if(((x<MAXX-3)&&(y<MAXY-3)) && (map[y][x]=='X') && (map[y+1][x+1]=='M') && (map[y+2][x+2]=='A') && (map[y+3][x+3]=='S')) count++;
-			if(((x>2)&&(y>2)) && (map[y][x]=='X') && (map[y-1][x-1]=='M') && (map[y-2][x-2]=='A') && (map[y-3][x-3]=='S')) count++;
-			if(((x<MAXX-3)&&(y>2)) && (map[y][x]=='X') && (map[y-1][x+1]=='M') && (map[y-2][x+2]=='A') && (map[y-3][x+3]=='S')) count++;
-			if(((x>2)&&(y<MAXY-3)) && (map[y][x]=='X') && (map[y+1][x-1]=='M') && (map[y+2][x-2]=='A') && (map[y+3][x-3]=='S')) count++;
-
-			if(oldc==count) printf("\033[0m");
-			else printf("\033[1;31m");
-			printf("%c",map[y][x]);
+	for(y=1; y<MAXY-1; y++) {
+		for(x=1; x<MAXX-1; x++) {
+			if(map[y][x]=='A') {
+				int nb;
+				for(nb=0;nb<8;nb+=2) {
+					if((mapnb(map,y,x,nb)=='M') && (mapnb(map,y,x,(nb+4)%8)=='S') &&
+					   (mapnb(map,y,x,(nb+2)%8)=='M') && (mapnb(map,y,x,(nb+6)%8)=='S')) count++;
+					if((mapnb(map,y,x,nb)=='M') && (mapnb(map,y,x,(nb+4)%8)=='S') &&
+					   (mapnb(map,y,x,(nb+2)%8)=='S') && (mapnb(map,y,x,(nb+6)%8)=='M')) count++;
+				}
+			}
 		}
-		printf("\n");
 	}
 
-	printf("%d\n",count);
-//	for(i=0; array[i]; i++) {
-//		printf("%d\n", array[i]);
-//	}
+	printf("%d\n",count/2); // The search does a full circle, hence every X is counted twice
 
 	return 0;
 }
