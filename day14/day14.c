fad50a00e16a4c489584184141c453c5f44e67ff
Zdenek Sustr <git@sustr.net>
2024-12-14 08:44:52 +0100

    Day 14, Task 2
    Don't expect this to work on different inputs.

--- day14/day14.c
+++ day14/day14.c
@@ -11,14 +11,6 @@
 #define MAXX 101
 #define MAXY 103
 #define MAXR 500
-//#define INPUT "unit1.txt"
-//#define MAXX 11
-//#define MAXY  7
-//#define MAXR 12
-//#define INPUT "unit2.txt"
-//#define MAXX 11
-//#define MAXY  7
-//#define MAXR  1
 
 // Point structure definition
 typedef struct {
@@ -28,39 +20,23 @@ typedef struct {
 	int dy;
 } TRobot;
 
-// Comparator function example
-int comp(const void *a, const void *b)
-{
-	const int *da = (const int *) a;
-	const int *db = (const int *) b;
-	return (*da > *db) - (*da < *db);
-}
-
-// Example for calling qsort()
-//qsort(array,count,sizeof(),comp);
-
-
 // Print a two-dimensional array
-void printMap (char **map) {
-	int x,y;
+void printMap (TRobot *robot) {
+	int x,y,r;
 	for(y=0; y<MAXY; y++) {
 		for(x=0; x<MAXX; x++) {
-			printf("%c", map[y][x]);
+			int dr=0;
+			for(r=0;r<MAXR;r++) {
+				if((y==robot[r].y)&&(x==robot[r].x)) {
+					dr=1;
+					break;
+				}
+			}
+			if(dr) printf("█");
+			else printf(" ");
 		}
 		printf("\n");
 	}
-}
-// Full block character for maps █ and border elements ┃━┗┛┏┓
-// Color printf("\033[1;31mR \033[1;32mG \033[1;34mB \033[0moff\n");
-
-// Retrieve nth neighbor from a map, diagonals are odd, side neighbors even
-int dy[] = { -1, -1, -1, 0, 1, 1,  1,  0};
-int dx[] = { -1,  0,  1, 1, 1, 0, -1, -1};
-char mapnb(char **map, int y, int x, int n) {
-	assert((n>=0) && (n<8));
-	if((y+dy[n]<0) || (y+dy[n]>=MAXY) ||
-	   (x+dx[n]<0) || (x+dx[n]>=MAXX)) return 0;
-	return(map[y+dy[n]][x+dx[n]]);
 }
 
 // Read input file line by line (e.g., into an array)
@@ -99,39 +75,25 @@ TRobot *readInput() {
 	return inst;
 }
 
-int count(TRobot* robot) {
-	int i;
-	int sum[] = {0,0,0,0};
-	for(i=0; i<MAXR; i++) {
-		printf("%d, %d\n", robot[i].x, robot[i].y);
-		if((robot[i].x<MAXX/2)&&(robot[i].y<MAXY/2)) sum[0]++;
-		else if((robot[i].x>MAXX/2)&&(robot[i].y<MAXY/2)) sum[1]++;
-		else if((robot[i].x<MAXX/2)&&(robot[i].y>MAXY/2)) sum[2]++;
-		else if((robot[i].x>MAXX/2)&&(robot[i].y>MAXY/2)) sum[3]++;
-	}
-	return sum[0]*sum[1]*sum[2]*sum[3];
-}
-
 int main(int argc, char *argv[]) {
 
 	TRobot *robot;
 	int i=0;	
 	robot = readInput();
 
-//	#pragma omp parallel
-	for(i=0; i<MAXR; i++) {
-		printf("%d, %d\n", robot[i].x, robot[i].y);
-		for(int sec=0;sec<100;sec++) {
+	int sec=0;
+	for(sec=0; sec<7569; sec++) {
+		for(i=0; i<MAXR; i++) {
 			robot[i].x=(robot[i].x+robot[i].dx);
 			while(robot[i].x>=MAXX) robot[i].x-=MAXX;
 			while(robot[i].x<0) robot[i].x+=MAXX;
 			robot[i].y=(robot[i].y+robot[i].dy);
 			while(robot[i].y>=MAXY) robot[i].y-=MAXY;
 			while(robot[i].y<0) robot[i].y+=MAXY;
-			printf("sec %d: %d, %d\n", sec, robot[i].x, robot[i].y);
 		}
 	}
+	printMap(robot);
+	printf("Seconds: %d\n", sec);
 
-	printf("Safety factor: %d\n", count(robot));
 	return 0;
 }
