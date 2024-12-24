43e199157c87a6986b0bddc65de914e5e5dc8b59
Zdenek Sustr <git@sustr.net>
2024-12-24 13:33:30 +0100

    All checks implemented (?)

--- day24/day24.c
+++ day24/day24.c
@@ -159,15 +159,171 @@ TComp *readInput(TRule *rule) {
 	return comp;
 }
 
+void printRule(TRule r, TComp *comp) {
+	printf("%d ", r.in[0]);
+	switch(r.type) {
+	case 2:
+		printf("AND ");
+		break;
+	case 3:
+		printf("OR ");
+		break;
+	case 4:
+		printf("XOR ");
+		break;
+	default:
+		printf("??? ");
+		break;
+	}
+
+	printf("%d -> %d", r.in[1], r.out);
+
+	printf(" | %s ", comp[r.in[0]].name);
+	switch(r.type) {
+	case 2:
+		printf("AND ");
+		break;
+	case 3:
+		printf("OR ");
+		break;
+	case 4:
+		printf("XOR ");
+		break;
+	default:
+		printf("??? ");
+		break;
+	}
+
+	printf("%s -> %s", comp[r.in[1]].name, comp[r.out].name);
+
+}
+
+int adder(int no, TComp *comp, TRule *rule, int *carry) {
+
+	int retval=1;
+
+	printf("Adder %d:\n", no);
+
+	int a,b;
+	for(a=0; comp[a].name; a++) if((comp[a].name[0]=='x')&&(atoi(comp[a].name+1)==no)) break;
+	for(b=0; comp[b].name; b++) if((comp[b].name[0]=='y')&&(atoi(comp[b].name+1)==no)) break;
+
+	printf("  Wire A %d (%s)\n", a, comp[a].name);
+	printf("  Wire B %d (%s)\n", b, comp[b].name);
+
+	int x1;
+	for(x1=0; rule[x1].type; x1++) {
+		if(rule[x1].type!=4) continue;
+		if(((rule[x1].in[0]==a)&&(rule[x1].in[1]==b)) ||
+		   ((rule[x1].in[0]==b)&&(rule[x1].in[1]==a))) break;
+	}
+	if(rule[x1].type!=4) {
+		retval=0;
+		printf("\033[1;31mInput %d not XORed\033[0m\n", no);
+	}
+
+	printf("    go into XOR %d (", x1);
+	printRule(rule[x1], comp);
+	printf(")\n");
+
+	int Ci1=-1;
+	int Ci2=-1;
+
+	if(no!=0) {
+		int x2;
+		for(x2=0; rule[x2].type; x2++) {
+			if(rule[x2].type!=4) continue;
+			if((rule[x2].in[0]==rule[x1].out)||(rule[x2].in[1]==rule[x1].out)) break;
+		}
+		if(rule[x2].type!=4) {
+			retval=0;
+			printf("\033[1;31m%d does not go to another XOR\033[0m\n", x2);
+		}
+		printf("      that goes into XOR %d (", x2);
+		printRule(rule[x2], comp);
+		printf(")\n");
+		Ci1 = rule[x2].in[0] == rule[x1].out ? rule[x2].in[1] : rule[x2].in[0];
+		printf("        together with Carry in %d\n", Ci1);
+	}
+
+
+
+	int a1;
+	for(a1=0; rule[a1].type; a1++) {
+		if(rule[a1].type!=2) continue;
+		if((rule[a1].in[0]==rule[x1].out)||(rule[a1].in[1]==rule[x1].out)) break;
+	}
+	if(rule[a1].type!=2) {
+		retval=0;
+		printf("\033[1;31mInput %d not ANDed\033[0m\n", no);
+	}
+	printf("      that goes into AND %d (", a1);
+	printRule(rule[a1], comp);
+	printf(")\n");
+	
+	Ci2 = rule[a1].in[0] == rule[x1].out ? rule[a1].in[1] : rule[a1].in[0];
+	printf("        together with Carry in %d\n", Ci2);
+
+	if((Ci1>=0)&&(Ci1!=Ci2)) {
+		retval=0;
+		printf("\033[1;31mCarry inputs do not match at %d\033[0m\n", no);
+	}
+
+	
+	int a2;
+	for(a2=0; rule[a2].type; a2++) {
+		if(rule[a2].type!=2) continue;
+		if(((rule[a2].in[0]==a)&&(rule[a2].in[1]==b)) ||
+		   ((rule[a2].in[0]==b)&&(rule[a2].in[1]==a))) break;
+	}
+	if(rule[a2].type!=2) {
+		retval=0;
+		printf("\033[1;31mDiff with carry at %d not ANDed\033[0m\n", no);
+	}
+	printf("    go into AND %d (", a2);
+	printRule(rule[a2], comp);
+	printf(")\n");
+
+	int o2;
+	for(o2=0; rule[o2].type; o2++) {
+		if(rule[o2].type!=3) continue;
+		if(((rule[o2].in[0]==rule[a1].out)&&(rule[o2].in[1]==rule[a2].out)) ||
+		   ((rule[o2].in[0]==rule[a2].out)&&(rule[o2].in[1]==rule[a1].out))) break;
+	}
+	if(rule[o2].type!=3) {
+		retval=0;
+		printf("\033[1;31m sum and carry at %d not ORed\033[0m\n", no);
+	}
+
+	printf("      that goes to OR %d (", o2);
+	printRule(rule[o2], comp);
+	printf(")\n");
+
+
+	int Co = rule[o2].out;
+	carry[no]=Co;
+	printf("        and produces Carry out %d (%s)\n", Co, comp[Co].name);
+	
+
+	if((no>0)&&(Ci2!=carry[no-1])) {
+		retval=0;
+		printf("\033[1;31m Wrong carry at %d. %d != %d \033[0m\n", no, Ci2, carry[no-1]);
+	}
+
+	return retval;	
+}
+
 int main(int argc, char *argv[]) {
 
-	int i=0;	
+//	int i=0;	
 //	array = readInput();
 	TRule *rule=(TRule*)calloc(MAXY, sizeof(TRule));
 	TComp *comp = readInput(rule);
 
+	int *carry=(int*)calloc(MAXX, sizeof(int));
+
 //	#pragma omp parallel for private(<uniq-var>) shared(<shared-var>)
-	while (1) {
+/*	while (1) {
 		int change=0;
 		for(i=0; rule[i].type; i++) {
 			int ret;
@@ -192,6 +348,11 @@ int main(int argc, char *argv[]) {
 
 	for(i=0; comp[i].name; i++) {
 		if(comp[i].name[0]=='z') printf("%s %d\n", comp[i].name, comp[i].out);
+	}*/
+
+	for(int q=0; q<45; q++) {
+		if(!adder(q, comp, rule, carry)); //break;
+		printf("\n");
 	}
 
 	return 0;
